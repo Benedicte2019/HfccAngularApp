08573b6dcd5cf6fa982fb1cd5c5ebe372157b5f7
joshua <mama27j@gmail.com>
2019-05-22 15:37:29 +0300

    add todo header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TodoHeaderComponent } from './todo-header/todo-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoHeaderComponent
   ],
   imports: [
     BrowserModule
