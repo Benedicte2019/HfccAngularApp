cef385b21c8bc9b9949708766952c52d08d266f1
Furaha <furaha@benedict.com>
2019-05-25 12:11:29 +0300

    I added a component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { TodoHeaderComponent } from './todo-header/todo-header.component';
 import { TodoInputComponent } from './todo-input/todo-input.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
+import { TaskComponent } from './task/task.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodoHeaderComponent,
     TodoInputComponent,
-    TodoListComponent
+    TodoListComponent,
+    TaskComponent
   ],
   imports: [
     BrowserModule
